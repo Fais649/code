661451c1dcab6a3c92a2c5cf37008c74063a54ad
Fais649 <xial649@gmail.com>
2024-08-31 14:22:52 +0200

    autocommit

---
+++ esp32/punk.os/include/event.h
@@ -0,0 +1,12 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <Arduino.h>
+
+struct {
+	String name;
+	
+} Event;
+
+
+#endif
---
+++ esp32/punk.os/include/state.h
@@ -0,0 +1,18 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <Arduino.h>
+#include <event.h>
+
+/*struct {*/
+/*	Event event;*/
+/*} State;*/
+/**/
+/*class StateSetter() {*/
+/*public:*/
+/*	void updateEvent(Event event);*/
+/*	void updateApp(App app);*/
+/*	Event *getEvent();*/
+/*};*/
+
+#endif
